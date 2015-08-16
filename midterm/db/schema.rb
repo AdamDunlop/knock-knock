06c2007eda6839dc017cfc63fa9834410a25573c
evertr <evertrotshuizen@gmail.com>
2015-08-16 02:36:30 +0000

    pushing before pulling - is that what she said?

--- midterm/db/schema.rb
+++ midterm/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814235234) do
+ActiveRecord::Schema.define(version: 20150815043235) do
 
   create_table "comedians", force: :cascade do |t|
     t.string   "first_name"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20150814235234) do
     t.datetime "updated_at"
     t.string   "email"
     t.string   "password"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "events", force: :cascade do |t|
